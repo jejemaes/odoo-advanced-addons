9a522dd4c698d43689f69e7060de97a7d7e06ab7
jem-odoo <jem@openerp.com>
2018-06-10 13:48:52 +0200

    [ADD] cloud_base: prevent update notification

---
+++ cloud_base/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Cloud Base',
+    'version': '1.0',
+    'summary': 'Cloud base customizations',
+    'sequence': 90,
+    'description': """
+Core mechanisms for global customization of Odoo standart
+- prevent update notification
+- ...
+    """,
+    'category': 'Hidden',
+    'depends': ['mail'],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+}
---
+++ cloud_base/models/update.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from odoo.models import AbstractModel
+from odoo import api
+
+_logger = logging.getLogger(__name__)
+
+
+class PublisherWarrantyContract(AbstractModel):
+    _inherit = "publisher_warranty.contract"
+
+    @api.multi
+    def update_notification(self, cron_mode=True):
+        """ Prevent the ping ! """
+        return False
