278996c57ced90fc84d958052d1c642d076ce0f4
Jerome <jerome.2611@gmail.com>
2020-01-01 21:41:31 +0100

    [IMP] website_gallery_google: update google base url of images when needed

--- website_gallery_google/models/gallery.py
+++ website_gallery_google/models/gallery.py
@@ -74,3 +74,63 @@ class GalleryImage(models.Model):
     _inherit = 'website.gallery.image'
 
     google_identifier = fields.Char("Google Media Identifier", index=True)
+    google_last_sync_date = fields.Datetime("Last Synchronization", readonly=True, help="Last update of baseUrl")
+    google_url_expired = fields.Boolean("Google Base Url is still valid", compute='_compute_google_url_expired', search='_search_google_url_expired')
+
+    @api.depends('gallery_id.gallery_type', 'attachment_id.type')
+    def _compute_image_urls(self):
+        super(GalleryImage, self)._compute_image_urls()
+        for image in self:
+            if image.gallery_type == 'google':
+                if image.type == 'binary':
+                    image.image_url = '/web/image/%s?unique=%s' % (image.attachment_id.id, image.attachment_id.checksum)
+                    image.image_small_url = '/web/image/%s?height=250&unique=%s' % (image.attachment_id.id, image.attachment_id.checksum)
+                    image.image_medium_url = '/web/image/%s?height=640&unique=%s' % (image.attachment_id.id, image.attachment_id.checksum)
+                else:
+                    image.image_url = '%s=h%s' % (image.attachment_id.url, 3200)
+                    image.image_small_url = '%s=h%s' % (image.attachment_id.url, 250)
+                    image.image_medium_url = '%s=h%s' % (image.attachment_id.url, 640)
+
+    @api.depends('google_last_sync_date')
+    def _compute_google_url_expired(self):
+        for image in self:
+            if image.google_last_sync_date:
+                image.google_url_expired = image.google_last_sync_date + timedelta(minutes=GOOGLE_PHOTOS_BASE_URL_LIFETIME) < fields.Datetime.now()
+            else:
+                image.google_url_expired = True
+
+    @api.model
+    def _search_google_url_expired(self, operator, value):
+        if operator in expression.NEGATIVE_TERM_OPERATORS:
+            value = not value
+        expired_domain = [('google_last_sync_date', '<', fields.Datetime.now() - timedelta(minutes=GOOGLE_PHOTOS_BASE_URL_LIFETIME))]
+        if value:
+            return expired_domain
+        return expression.distribute_not(expired_domain)
+
+    def _google_update_url(self):
+        """ Update the base url with a valid google url if needed. The url will be fetched and saved only if the url is expired. """
+        expired_images = self.filtered(lambda img: img.google_url_expired)
+        if not expired_images:
+            return False
+
+        websites = expired_images.mapped('gallery_id.website_id')
+        grouped_by_website = dict.fromkeys(websites, self.env['website.gallery.image'])
+        for image in expired_images:
+            grouped_by_website[image.gallery_id.website_id] |= image
+
+        for website, images in grouped_by_website.items():
+            # fetch new google urls
+            expired_images_gid = images.mapped('google_identifier')
+            media_items = self.env['google.api'].sudo(website.user_id)._photos_batch_get_media_items(expired_images_gid)
+            gid_url_map = {item['id']: item['baseUrl'] for item in media_items}
+
+            # store them on the attachment
+            for image in images.sudo():
+                base_url = gid_url_map.get(image.google_identifier)
+                if base_url:
+                    image.write({
+                        'url': base_url,
+                        'google_last_sync_date': fields.Datetime.now(),
+                    })
+        return True
