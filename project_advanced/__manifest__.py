fbe81e08593dd9f55481f00f58ba7d4c6621f1c7
Jerome <jerome.2611@gmail.com>
2020-01-10 11:09:14 +0100

    [ADD] project_advanced: gantt for tasks

---
+++ project_advanced/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Project Advanced",
+    'summary': """Gantt View for Tasks, ... """,
+    'description': """
+
+    """,
+    "category": "Project",
+    'author': "jejemaes",
+    'license': "GPL-3",
+    'version': '1.1',
+
+    'depends': ['web_view_gantt', 'project'],
+    'data': [
+        'views/project_views.xml',
+    ],
+}
