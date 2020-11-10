72f20348a94670cb73fe13087ebd425337be4e33
Jerome <jerome.2611@gmail.com>
2020-11-10 23:20:21 +0100

    [SPLIT] project_advanced into project_template and project_gantt
    This idea is to have more simple addon; each one is responsible for an additionnal
    feature. '_advanced' should be used to add features to native odoo addons (without
    any other dependency).

--- project_advanced/models/res_config_settings.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, fields, models
-
-
-class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
-
-    group_project_template = fields.Boolean("Use Project Template", implied_group='project_advanced.group_project_template', group="project.group_project_user")
---
+++ project_template/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Project Template",
+    'summary': """Template for Projects""",
+    'description': """
+
+    """,
+    "category": "Project",
+    'author': "jejemaes",
+    'license': "GPL-3",
+    'version': '1.1',
+
+    'depends': ['project'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/project_views.xml',
+        'views/project_template_views.xml',
+    ],
+}
