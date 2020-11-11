599a8aa61a3182938d7573c58ec9e0e03d2e804e
Jerome <jerome.2611@gmail.com>
2020-11-11 14:24:20 +0100

    [ADD] event_project,event_project_template: event generate project
    Check the 'create project' option on event template, and the event will generate an
    project on event creation. This creation can be base on a project template. Also, the
    user will have to set the policy to determine the project start and stop dates.
    
    The purpose is to setup a project in order to organize the event preparation through
    tasks.

---
+++ event_project_template/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Event Project Template',
+    'category': 'Marketing/Events',
+    'summary': 'Generate a project from a Template',
+    'description': """
+
+""",
+    'depends': ['event_project', 'project_template'],
+    'data': [
+        'views/event_type_views.xml',
+    ],
+    'auto_install': True,
+}
