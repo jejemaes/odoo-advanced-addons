1e191125a4e3e64a5ff7c1f4422b053962af69d3
Jerome <jerome.2611@gmail.com>
2020-11-11 14:21:47 +0100

    [ADD] event_analytic: create analytic account with template
    Check the tracking costs option on event template, and the event will generate an
    analytic account on event creation. The purpose is to track expenses, timesheets,
    invoices, purchases, ... related to the event.

---
+++ event_analytic/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Event Analytic',
+    'category': 'Marketing/Events',
+    'summary': 'Track Costs of Events',
+    'description': """
+
+""",
+    'depends': ['event', 'analytic'],
+    'data': [
+        'views/event_type_views.xml',
+        'views/event_views.xml',
+    ],
+}
