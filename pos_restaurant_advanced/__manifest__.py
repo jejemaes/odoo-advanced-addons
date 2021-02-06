bc6c9c137c41d3d7f9369df9ec1352b3d95ed042
Jerome <jerome.2611@gmail.com>
2021-02-06 21:51:32 +0100

    [ADD] pos_restaurant_advanced: new module
    This module provides a wizard to generate tables for a given
    floor. It speeds up table creation.

---
+++ pos_restaurant_advanced/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : 'PoS Restaurant Advanced',
+    'version' : '1.0',
+    'category': 'Sales/Point of Sale',
+    'sequence': 80,
+    'description': """
+PoS Restaurant Advanced
+=======================
+Wizard to create multiple table on a floor plan
+
+    """,
+    'depends' : ['pos_restaurant'],
+    'data': [
+        'security/ir.model.access.csv',
+        'wizard/restaurant_table_generator_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
