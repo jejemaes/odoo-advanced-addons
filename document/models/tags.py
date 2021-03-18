9f076446cb0476f52fd42ffd35be37fabd909398
Jerome <jerome.2611@gmail.com>
2021-03-18 23:09:57 +0100

    [IMP] document: big code cleaning
    - avoid onchange returning domain
    - change color field of tag (color by facet)
    - fix issue when uploading document on share directory
    - relabelling and UI fine tuning
    - ...

--- document/models/tags.py
+++ document/models/tags.py
@@ -10,10 +10,11 @@ class Categories(models.Model):
     _order = "sequence, name"
 
     name = fields.Char("Name", required=True, translate=True)
-    folder_id = fields.Many2one('document.folder', string="Folder", required=True, ondelete="cascade")
+    folder_id = fields.Many2one('document.folder', string="Folder", required=False, ondelete="cascade")
     tag_ids = fields.One2many('document.tag', 'facet_id', string="Tags")
     tooltip = fields.Char("Tooltip", help="Hover Text Description")
     sequence = fields.Integer("Sequence", default=10)
+    color = fields.Integer("Color", default=0)
 
     _sql_constraints = [
         ('name_unique', 'UNIQUE(folder_id, name)', "Facet already exists in this folder"),
@@ -29,17 +30,16 @@ class Tags(models.Model):
     folder_id = fields.Many2one('document.folder', string="Folder", related='facet_id.folder_id', store=True, readonly=False)
     facet_id = fields.Many2one('document.facet', string="Category", ondelete='cascade', required=True)
     sequence = fields.Integer("Sequence", default=10)
-    color = fields.Integer("Color", default=0)
+    color = fields.Integer("Color", related='facet_id.color', readonly=False, store=True)
 
     _sql_constraints = [
         ('facet_name_unique', 'UNIQUE (facet_id, name)', "Tag already exists for this facet"),
     ]
 
     def name_get(self):
-        if self._context.get('document_tag_simple_name'):
-            return super(Tags, self).name_get()
-
-        result = []
-        for record in self:
-            result.append((record.id, "%s: %s" % (record.facet_id.name, record.name)))
-        return result
+        if self._context.get('document_tag_complete_name'):
+            result = []
+            for record in self:
+                result.append((record.id, "%s: %s" % (record.facet_id.name, record.name)))
+            return result
+        return super(Tags, self).name_get()
