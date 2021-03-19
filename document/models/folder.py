bfa80b05f546ea7d2712babe8b3cf0c50a970d21
Jerome <jerome.2611@gmail.com>
2021-03-20 00:09:26 +0100

    [IMP] document: add folder hierachy
    Adds a kanban view to naviguation through folders like
    directories, showing number of sub-folders and documents
    inside.

--- document/models/folder.py
+++ document/models/folder.py
@@ -28,6 +28,7 @@ class Folder(models.Model):
 
     parent_id = fields.Many2one('document.folder', string="Parent Folder", ondelete="cascade", check_company=True, help="Tag categories from parent folders will be shared to their sub folders")
     children_ids = fields.One2many('document.folder', 'parent_id', string="Sub-folders")
+    children_count = fields.Integer("Sub-folder Count", compute='_compute_children_count')
 
     facet_ids = fields.One2many('document.facet', 'folder_id', string="Tag Categories", help="Select the tag categories to be used")
     read_group_ids = fields.Many2many('res.groups', 'document_folder_read_group_rel', string="Read Access Groups", help="Groups able to see the workspace and read its documents without create/edit rights.")
@@ -39,6 +40,14 @@ class Folder(models.Model):
         document_count_dict = dict((d['folder_id'][0], d['folder_id_count']) for d in data)
         for folder in self:
             folder.document_count = document_count_dict.get(folder.id, 0)
+
+    @api.depends('children_ids')
+    def _compute_children_count(self):
+        data = self.env['document.folder'].read_group([('parent_id', 'in', self.ids)], fields=['parent_id'], groupby=['parent_id'])
+        children_count_dict = dict((d['parent_id'][0], d['parent_id_count']) for d in data)
+        for folder in self:
+            folder.children_count = children_count_dict.get(folder.id, 0)
+
 
     @api.constrains('parent_id')
     def _check_parent_id(self):
@@ -53,3 +62,22 @@ class Folder(models.Model):
             else:
                 result.append((record.id, record.name))
         return result
+
+    def action_view_subfolders(self):
+        action = self.env.ref('document.document_folder_action_directories').read()[0]
+        action['display_name'] = self.name
+        action['domain'] = False
+        action['context'] = {
+            'search_default_parent_id': self.id,
+            'default_parent_id': self.id,
+        }
+        return action
+
+    def action_view_documents(self):
+        action = self.env.ref('document.document_document_action').read()[0]
+        action['display_name'] = self.name
+        action['context'] = {
+            'search_default_folder_id': self.id,
+            'default_folder_id': self.id,
+        }
+        return action
