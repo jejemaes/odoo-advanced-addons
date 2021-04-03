af09e4c1b1e0c8780733eab59664394a1e8e03ad
Jerome <jerome.2611@gmail.com>
2021-04-03 10:23:32 +0200

    [ADD] event_document: handle document for events
    We can now:
    - centralize all documents related to events in a dedicated folder
    - default tags for uploads on events are added
    - have a specific subfolder per event (depending on template and
    stage configuration)
    - addtionnal tags can be managed from the event

---
+++ event_document/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Document in Event",
+    'summary': "Document Management System, in Events",
+    'description': """
+        Upload, manage and share your Documents related to Events
+    """,
+    'author': "jejemaes",
+    'category': 'Extra Tools',
+    'version': '1.0',
+    'application': False,
+    'depends': ['document', 'event_advanced'],
+    'auto_install': True,
+    'data': [
+        'security/event_document_security.xml',
+        'views/res_config_settings_views.xml',
+        'views/event_event_views.xml',
+    ],
+}
