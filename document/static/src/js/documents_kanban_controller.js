76de0d0d7060f21ce80e10c94e37aa8a1aa63f4d
Jerome <jerome.2611@gmail.com>
2021-04-12 22:47:32 +0200

    [IMP] document: adaptation for futur modules
    As we want to handle different document
    type, the base module required some
    adaptations. Here are they.

--- document/static/src/js/documents_kanban_controller.js
+++ document/static/src/js/documents_kanban_controller.js
@@ -13,6 +13,10 @@ var DocumentViewer = MailDocumentViewer.extend({
 });
 
 var DocumentsKanbanController = KanbanController.extend({
+    buttons_template: 'DocumentKanbanView.buttons',
+    events: Object.assign({}, KanbanController.prototype.events, {
+        'click a.o-kanban-button-new-document': '_onCreateNewDocument',
+    }),
     custom_events: Object.assign({}, KanbanController.prototype.custom_events, {
         kanban_image_clicked: '_onKanbanImageClicked'
     }),
@@ -30,6 +34,32 @@ var DocumentsKanbanController = KanbanController.extend({
         const recordId = ev.data.recordId;
         const documentViewer = new DocumentViewer(this, documents, recordId);
         await documentViewer.appendTo(this.$('.o_documents_view'));
+    },
+
+    renderButtons: function ($node) {
+        this._super(...arguments);
+
+        // hide dropdown if empty
+        if (this.$buttons.find('.o_document_kanban_btn_list_dropdown > a').length === 1) {
+            this.$buttons.find('#o_document_kanban_btn_list').hide();
+        } else {
+            this.$buttons.on('click', '.o_document_kanban_btn_list_dropdown > a.o-kanban-button-new', this._onButtonNew.bind(this));
+        }
+    },
+    /**
+     * Handler to open form view with correct default document handler in context
+     *
+     **/
+    _onCreateNewDocument: function(ev) {
+        const handler = $(ev.currentTarget).data('handler');
+        this.do_action({
+            type: 'ir.actions.act_window',
+            views: [[false, 'form']],
+            res_model: this.modelName,
+            context: {
+                default_handler: handler,
+            }
+        });
     },
 
 });
