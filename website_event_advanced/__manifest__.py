6190a90aa875e57fde79452992c48e9e79c12cce
Jerome <jerome.2611@gmail.com>
2021-04-27 22:44:36 +0200

    event_advanced: allow event without registration
    Registration should be an option, if we want to only expose
    public event on our website, without having the registration
    notion (seats, limitation, mail to send, ...).

--- website_event_advanced/__manifest__.py
+++ website_event_advanced/__manifest__.py
@@ -1,21 +1,19 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': "Event Advanced",
-    'summary': "Advanced options for Events",
+    'name': "Website Event Advanced",
+    'summary': "Advanced options for Website Event",
     'description': """
-        Advanced options for Events:
-        - Allow to disable registration
-        - ...
+        Advanced options for Events
     """,
     'author': "jejemaes",
     'category': 'Marketing/Events',
     'version': '1.0',
     'application': False,
-    'depends': ['event'],
+    'depends': ['event_advanced', 'website_event'],
     'auto_install': True,
     'data': [
         'views/event_event_views.xml',
-        'views/event_type_views.xml',
+        'views/event_templates.xml',
     ],
 }
