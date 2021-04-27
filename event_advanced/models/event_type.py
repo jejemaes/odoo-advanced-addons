148712cd2f05dc29b1a1978998e6dcca1db000ed
Jerome <jerome.2611@gmail.com>
2021-04-27 22:44:36 +0200

    event_advanced: allow event without registration
    Registration should be an option, if we want to only expose
    public event on our website, without having the registration
    notion (seats, limitation, mail to send, ...).

---
+++ event_advanced/models/event_type.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+
+
+class EventType(models.Model):
+    _inherit = 'event.type'
+
+    use_registration = fields.Boolean("Registration", default=True, help="Check this to allow people to register to the event and activate the attendees management")
+
+    @api.onchange('use_registration')
+    def _onchange_use_registration(self):
+        if not self.use_registration:
+            self.use_ticket = False
