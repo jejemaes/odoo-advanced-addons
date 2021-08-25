7aad871cebdb63977aa58c449895a4bdd9f87d5c
Jerome <jerome.2611@gmail.com>
2021-08-25 14:11:54 +0200

    [IMP][REF] pos_restaurant_kitchen: allow print per kitchen
    - receipt rendered server side
    - kitchen can see all order lines and print the entier order
    from it auto refresh kanban
    - refresh time can be set up
    - skeleton for subset of order line is not implemented
    - only print from the local printer (connected to compute) is possible

--- pos_restaurant_kitchen/models/__init__.py
+++ pos_restaurant_kitchen/models/__init__.py
@@ -2,4 +2,3 @@
 from . import pos_restaurant_kitchen
 from . import pos_config
 from . import pos_order
-from . import res_company
---
+++ pos_restaurant_kitchen/static/src/js/print_widget.js
@@ -0,0 +1,68 @@
+odoo.define('pos_restaurant_kitchen.PrintWidget', function (require) {
+"use strict";
+
+var AbstractField = require('web.AbstractField');
+
+const session = require('web.session');
+var field_registry = require('web.field_registry');
+
+
+
+
+var PrintWidget = AbstractField.extend({
+    events: _.extend({}, AbstractField.prototype.events, {
+        'click': '_onClick'
+    }),
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * Display button
+     * @override
+     * @private
+     */
+    _render: function () {
+        if (this.value) {
+            this.$el.html('<button class="btn btn-primary"><i class="fa fa-print"/>Print</button>');
+            this.$el.attr('title', this.value);
+            this.$el.attr('aria-label', this.value);
+        }
+    },
+
+    //--------------------------------------------------------------------------
+    // Handlers
+    //--------------------------------------------------------------------------
+
+    /**
+     * Open link button
+     *
+     * @private
+     * @param {MouseEvent} event
+     */
+    _onClick: function (event) {
+        event.stopPropagation();
+        var PrintWindow = window.open(this.value, '_new');
+
+        return;
+        var Pagelink = "about:blank";
+        var pwa = window.open(Pagelink, "_new");
+        pwa.document.open();
+        pwa.document.write(this._getHtmlPrintPage(this.value));
+        pwa.document.close();
+    },
+    _getHtmlPrintPage: function(source) {
+        return "<html><head><script>function step1(){\n" +
+                "setTimeout('step2()', 10);}\n" +
+                "function step2(){window.print();window.close()}\n" +
+                "</script></head><body onload='step1()'>\n" +
+                "<img src='" + source + "' /></body></html>";
+    }
+});
+
+
+field_registry.add('restaurant_print', PrintWidget);
+
+return PrintWidget;
+
+});
