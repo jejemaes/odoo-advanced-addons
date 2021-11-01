8f446a6b242839e62f19aa3af8fbb113a3bdadc1
Jerome <jerome.2611@gmail.com>
2021-11-01 15:42:25 +0100

    [ADD] base_georouting: distance API for routing computation
    This module extends the geolocater model to implement routing part of
    Geo Provider (openstreetmaps and googlemaps).
    Purpose is to compute the distance using routes between 2 addresses.

---
+++ base_georouting/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Base Geo Routing",
+    'description': """
+
+    """,
+    'category': 'Hidden/Tools',
+    'sequence': 330,
+    'version': '1.0',
+    'depends': ['base_geolocalize'],
+    'data': [],
+}
