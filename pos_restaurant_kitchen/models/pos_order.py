1268e992d2ceb647f8d5cf4137d32ef0afb75d83
Jerome <jerome.2611@gmail.com>
2021-12-14 21:38:26 +0100

    [IMP] pos_restaurant_kitchen: multi bar handeling
    The kitchen dashboard now display the relevant parts of the order, depending
    of its setup (all product or by category).
    Each kitchen has its own status: the m2m between pos.order and
    restaurant.kitchen is enriched.

--- pos_restaurant_kitchen/models/pos_order.py
+++ pos_restaurant_kitchen/models/pos_order.py
@@ -6,15 +6,69 @@ from functools import partial
 
 from odoo import api, fields, models, SUPERUSER_ID
 from odoo.osv import expression
+from odoo.exceptions import UserError
+
+
+class PosOrderPrint(models.Model):
+    _name = 'pos.order.kitchen.status'
+    _description = 'Order Kitchen Status'
+    _table = 'pos_order_kitchen_status'
+
+    kitchen_id = fields.Many2one('restaurant.kitchen', string="Kitchen", required=True)
+    order_id = fields.Many2one('pos.order', string="PoS Order", required=True)
+    print_count = fields.Integer("Print Counter", default=0)
 
 
 class PosOrder(models.Model):
     _inherit = 'pos.order'
 
-    print_count = fields.Integer("Print Counter", default=0)
-    print_url = fields.Char(compute='_compute_print_url')
-    kitchen_ids = fields.Many2many('restaurant.kitchen', 'pos_order_kitchen_rel', 'order_id', 'kitchen_id', string="Kitchens", readonly=True)
     kitchen_order_details = fields.Html("Order Details", compute='_compute_kitchen_order_details')
+
+    kitchen_status_ids = fields.One2many('pos.order.kitchen.status', 'order_id', string="Kitchen Status", depends=['kitchen_ids'])
+    kitchen_ids = fields.Many2many('restaurant.kitchen', 'pos_order_kitchen_status', 'order_id', 'kitchen_id', string="Involved Kitchens", readonly=True, help="")
+    print_count = fields.Integer("Print Counter", compute='_compute_print_count', inverse='_inverse_print_count', search='_search_print_count', store=False)
+    print_url = fields.Char(compute='_compute_print_url')
+
+    @api.depends('kitchen_status_ids.print_count')
+    @api.depends_context('kitchen_id')
+    def _compute_print_count(self):
+        kitchen_id = self.env.context['kitchen_id']
+        status_list = self.env['pos.order.kitchen.status'].search([('order_id', 'in', self.ids), ('kitchen_id', '=', kitchen_id)])
+        status_map = {status.order_id.id: status.print_count for status in status_list}
+        for order in self:
+            order.print_count = status_map.get(order.id, 0)
+
+    def _inverse_print_count(self):
+        kitchen_id = self.env.context['kitchen_id']
+        status_list = self.env['pos.order.kitchen.status'].search([('order_id', 'in', self.ids), ('kitchen_id', '=', kitchen_id)])
+        status_map = {status.order_id.id: status for status in status_list}
+        status_to_create = []
+        for order in self:
+            status = status_map.get(order.id)
+            if status:
+                status.print_count = order.print_count
+            else:
+                status_to_create.append({
+                    'kitchen_id': kitchen_id,
+                    'order_id': order.id,
+                    'print_count': order.print_count
+                })
+        if status_to_create:
+            self.env['pos.order.kitchen.status'].create(status_to_create)
+
+    def _search_print_count(self, operator, value):
+        kitchen_id = self.env.context['kitchen_id']
+
+        if operator not in expression.TERM_OPERATORS:  # prevent sql injection
+            raise UserError("Invalide operator.")
+
+        query = """
+            SELECT S.order_id
+            FROM %s S
+            WHERE kitchen_id = %%s AND print_count %s %%s
+        """ % (self.env['pos.order.kitchen.status']._table, operator)
+
+        return [('id', 'inselect', (query, (kitchen_id, value)))]
 
     @api.depends_context('kitchen_id')
     def _compute_print_url(self):
@@ -31,66 +85,58 @@ class PosOrder(models.Model):
             order.kitchen_order_details = self.env["ir.ui.view"]._render_template('pos_restaurant_kitchen.pos_order_reprint_receipt_order_line', {'lines': lines})
 
     @api.model
+    def create(self, values):
+        order = super(PosOrder, self).create(values)
+
+        if order.config_id.is_kitchen_preparation:
+            # extract categories
+            pos_categories = order.lines.mapped('product_id.pos_categ_id')
+            # select kitchen from the config that are involved in at least one pos line
+            kitchen_ids = []
+            for kitchen in order.config_id.kitchen_ids:
+                if kitchen.order_line_mode == 'all':
+                    kitchen_ids.append(kitchen.id)
+                elif kitchen.order_line_mode == 'category':
+                    if set(kitchen.pos_category_ids.ids) & set(pos_categories.ids):
+                        kitchen_ids.append(kitchen.id)
+            # create kitchen status here, in order to having the default value set (m2m model problem)
+            if kitchen_ids:
+                value_to_create = []
+                for kitchen_id in kitchen_ids:
+                    value_to_create.append({
+                        'order_id': order.id,
+                        'kitchen_id': kitchen_id,
+                    })
+                self.env['pos.order.kitchen.status'].sudo().create(value_to_create)
+
+        return order
+
+    @api.model
     def _complete_values_from_session(self, session, values):
         values = super()._complete_values_from_session(session, values)
 
         if session.config_id.is_kitchen_preparation:
-            kitchen_ids = session.config_id.kitchen_ids.filtered(lambda k: k.order_line_mode == 'all').ids
+            # extract product of lines to be created
+            product_ids = []
+            for cmd in values.get('lines', []):
+                if len(cmd) == 3:
+                    product_ids.append(cmd[2]['product_id'])
+            pos_categories = self.env['product.product'].browse(product_ids).mapped('pos_categ_id')
+
+            # select kitchen from the config that are involved in at least one pos line
+            kitchen_ids = []
+            for kitchen in session.config_id.kitchen_ids:
+                if kitchen.order_line_mode == 'all':
+                    kitchen_ids.append(kitchen.id)
+                elif kitchen.order_line_mode == 'category':
+                    if set(kitchen.pos_category_ids.ids) & set(pos_categories.ids):
+                        kitchen_ids.append(kitchen.id)
+
             if kitchen_ids:
                 values['kitchen_ids'] = [(6, 0, kitchen_ids)]
 
         return values
 
-
-# class PosOrderLine(models.Model):
-#     _inherit = 'pos.order.line'
-
-#     @api.model
-#     def _default_kitchen_stage_id(self):
-#         return self.env['restaurant.kitchen.stage'].search([], limit=1)
-
-#     order_id = fields.Many2one(readonly=True)
-#     config_id = fields.Many2one('pos.config', string="Point of Sale", related='order_id.config_id')
-#     table_id = fields.Many2one('restaurant.table', related='order_id.table_id')
-#     user_id = fields.Many2one('res.users', string="User", related='order_id.user_id')
-
-#     kitchen_ids = fields.Many2many('restaurant.kitchen', 'pos_order_line_kitchen_rel', 'order_line_id', 'kitchen_id', string="Kitchens", readonly=True)
-#     kitchen_stage_id = fields.Many2one(
-#         'restaurant.kitchen.stage', string='Preparation Stage', index=True, default=_default_kitchen_stage_id,
-#         copy=False, group_expand='_read_group_stage_ids', ondelete='set null')
-#     kitchen_stage_closed = fields.Boolean(related='kitchen_stage_id.is_closed', search='_search_kitchen_stage_closed')
-#     kitchen_stage_color = fields.Integer(related='kitchen_stage_id.color')
-#     kitchen_stage_button_text = fields.Char(related='kitchen_stage_id.button_text')
-
-#     @api.model
-#     def _read_group_stage_ids(self, stages, domain, order):
-#         stage_ids = stages._search([], order=order)
-#         return stages.browse(stage_ids)
-
-#     def _search_kitchen_stage_closed(self, operator, value):
-#         closed_stages = self.env['restaurant.kitchen.stage'].search([('is_closed', '=', True)])
-#         if operator in expression.NEGATIVE_TERM_OPERATORS:
-#             new_operator = 'not in' if value else 'in'
-#         else:
-#             new_operator = 'in' if value else 'not in'
-#         return [('kitchen_stage_id', new_operator, closed_stages.ids)]
-
-#     def _order_line_fields(self, line, session_id=None):
-#         """ Hook to add preparation IDs, for the line to be visible in the preparation. """
-#         line = super(PosOrderLine, self)._order_line_fields(line, session_id=session_id)
-#         if session_id:
-#             session = self.env['pos.session'].browse(session_id).exists() if session_id else None
-#             if session and session.config_id.kitchen_ids:
-#                 product = self.env['product.product'].browse(line[2]['product_id'])
-#                 kitchen_ids = [kitchen.id for kitchen in session.config_id.kitchen_ids if product.pos_categ_id in kitchen.pos_category_ids]
-#                 line[2]['kitchen_ids'] = [(6, 0, kitchen_ids)]
-#         return line
-
-#     def action_next_stage(self):
-#         if self.kitchen_stage_id:
-#             kitchen_stage = self.env['restaurant.kitchen.stage'].search([('sequence', '>', self.kitchen_stage_id.sequence)], limit=1)
-#             self.write({'kitchen_stage_id': kitchen_stage.id})
-
-#     def action_reset(self):
-#         kitchen_stage = self.env['restaurant.kitchen.stage'].search([], limit=1)
-#         self.write({'kitchen_stage_id': kitchen_stage.id})
+    def action_mark_as_print(self):
+        self.sudo().write({'print_count': -1})
+        return True
