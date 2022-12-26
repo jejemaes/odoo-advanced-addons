c9db416ea5d037a43dae7f946e6d158a8718460e
Jerome Maes <jerome.2611@gmail.com>
2022-12-26 12:37:37 +0100

    [MIGR] cloud_worker: Adaptation to 16.0 code
    Changes due to HTTPocalypse https://github.com/odoo/odoo/pull/78857 That changes everything in handling request and database.

--- cloud_worker/__init__.py
+++ cloud_worker/__init__.py
@@ -9,9 +9,15 @@ from odoo.tools import config
 _base_db_filter = http.db_filter
 
 
-def db_filter_headers(dbs, httprequest=None):
-    # check the header of the request (prior to old system)
-    httprequest = httprequest or request.httprequest
+def db_filter_headers(dbs, host=None):
+    """
+    :param Iterable[str] dbs: The list of database names to filter.
+    :param host: The Host used to replace %h and %d in the dbfilters
+        regexp. Taken from the current request when omitted.
+    :returns: The original list filtered.
+    :rtype: List[str]
+    """
+    httprequest = request.httprequest
     db_header = httprequest.headers.get('X-Odoo-Dbname')
     if db_header and db_header in dbs:
         return [db_header]
