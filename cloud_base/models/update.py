7dd4c8576415f89e412774b50946df21ab41445f
jejemaes <jerome.2611@gmail.com>
2023-12-03 18:45:10 +0100

    [FIX] cloud_base: remove api.multi

--- cloud_base/models/update.py
+++ cloud_base/models/update.py
@@ -11,7 +11,6 @@ _logger = logging.getLogger(__name__)
 class PublisherWarrantyContract(AbstractModel):
     _inherit = "publisher_warranty.contract"
 
-    @api.multi
     def update_notification(self, cron_mode=True):
         """ Prevent the ping ! """
         return False
