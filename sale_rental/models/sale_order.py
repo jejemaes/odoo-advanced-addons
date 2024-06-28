2e764b60b21162967121986a1966391980e88940
jejemaes <jerome.2611@gmail.com>
2024-06-28 09:23:10 +0200

    fixup migr sale_rental

--- sale_rental/models/sale_order.py
+++ sale_rental/models/sale_order.py
@@ -36,8 +36,8 @@ class SaleOrder(models.Model):
             'type': 'ir.actions.act_window',
             'name': _('Rental Bookings'),
             'domain': [('sale_line_id', 'in', self.order_line.ids)],
-            'views': [(False, 'gantt'), (False, 'kanban'), (False, 'tree'), (False, 'form')],
-            'view_mode': 'gantt,kanban,tree,form',
+            'views': [(False, 'calendar'), (False, 'gantt'), (False, 'kanban'), (False, 'tree'), (False, 'form')],
+            'view_mode': 'calendar,gantt,kanban,tree,form',
             'res_model': 'rental.booking',
         }
 
